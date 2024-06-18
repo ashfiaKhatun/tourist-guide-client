391cd77bd14df455fc8884f1f2cad069554bd61c
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-18 23:52:32 +0600

    dashboard file route added

--- src/routes/PublicRouter.jsx
+++ src/routes/PublicRouter.jsx
@@ -7,6 +7,14 @@ import ErrorPage from "../pages/ErrorPage";
 import SignIn from "../pages/SignIn/SignIn";
 import SignUp from "../pages/SignUp/SignUp";
 import Dashboard from "../layout/Dashboard";
+import AdminProfile from "../pages/Dashboard/Admin/AdminProfile/AdminProfile";
+import AddPackage from "../pages/Dashboard/Admin/AddPackage/AddPackage";
+import ManageUsers from "../pages/Dashboard/Admin/ManageUsers/ManageUsers";
+import MyProfile from "../pages/Dashboard/TourGuide/MyProfile/MyProfile";
+import MyAssignedTours from "../pages/Dashboard/TourGuide/MyAssignedTours/MyAssignedTours";
+import TouristProfile from "../pages/Dashboard/Tourist/TouristProfile/TouristProfile";
+import MyBookings from "../pages/Dashboard/Tourist/MyBookings/MyBookings";
+import MyWishlist from "../pages/Dashboard/Tourist/MyWishlist/MyWishlist";
 
 const router = createBrowserRouter([
     {
@@ -34,9 +42,43 @@ const router = createBrowserRouter([
         path: 'dashboard',
         element: <Dashboard></Dashboard>,
         children: [
+
+            // Admin Route
             {
-                path: '/',
-                element: <Home></Home>
+                path: 'admin-profile',
+                element: <AdminProfile></AdminProfile>
+            },
+            {
+                path: 'add-package',
+                element: <AddPackage></AddPackage>
+            },
+            {
+                path: 'manage-users',
+                element: <ManageUsers></ManageUsers>
+            },
+
+            // Tour Guide Route
+            {
+                path: 'guide-profile',
+                element: <MyProfile></MyProfile>
+            },
+            {
+                path: 'assign-tour',
+                element: <MyAssignedTours></MyAssignedTours>
+            },
+
+            // Tourist Route
+            {
+                path: 'tourist-profile',
+                element: <TouristProfile></TouristProfile>
+            },
+            {
+                path: 'bookings',
+                element: <MyBookings></MyBookings>
+            },
+            {
+                path: 'wishlist',
+                element: <MyWishlist></MyWishlist>
             },
         ]
     },
